dd9b23dec8cbd7c7ca5d680f8fc551a8dfd2168a
MareRamirez <mramirez2019@udec.cl>
2023-11-24 22:33:04 -0300

    intento 1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,46 +1,88 @@
 #Margareth Ramirez Valenzuela, Ana María Vargas
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+import matplotlib.pylab as pylab
 from skimage import filters
+from scipy.ndimage import filters
 
 # Parte 1.A
 # Cargue y despliegue la imagen en escala de grises
 img = cv2.imread('moon.png',0)
-plt.imshow(img, cmap='gray')
+plt.imshow(img, cmap=pylab.cm.Greys_r)
 plt.title('Imagen original')
 plt.axis('off')
 plt.show()
+ 
+print(img.shape)
 
 # Parte 1.B
 # Transformada de Fourier
-img_fft = np.fft.fft2(img)
-# Centrar el espectro de frecuencia
-img_fft_shift = np.fft.fftshift(img_fft)
-# Espectro de amplitud
-img_fft_abs = np.abs(img_fft_shift)
-# Espectro de amplitud en escala logarítmica
-img_fft_log = np.log(img_fft_abs)
-
-# Despliegue del espectro de frecuencia centrado
-plt.imshow(img_fft_log, cmap='gray')
-plt.title('Espectro de frecuencia centrado')
+img_fft = np.fft.fft2(img) # Fourier
+img_fft_shift = np.fft.fftshift(img_fft) # Desplazamiento
+img_fft_abs = np.abs(img_fft_shift) # Magnitud
+img_fft_log = np.log(img_fft_abs) # Logaritmo
+print(img_fft_log.shape)
+plt.imshow(img_fft_log, cmap=pylab.cm.Greys_r)
+plt.title('Espectro de frecuencia')
 plt.axis('off')
 plt.show()
 
+
 # Parte 1.C
 # Para lograr eliminar el ruido en la imagen ocasionado por las frecuencias especıficas, primero
-# obtenga una mascara a partir del modulo de la Transformada de Fourier en escala logar ́ıtmica
-# utilizando un umbral igual a 10. Para unir los puntos mas cercanos aplique un filtro de
-# mınimo.
+# obtenga una mascara a partir del m ́odulo de la Transformada de Fourier en escala logarıtmica
+# utilizando un umbral igual a 10. Para unir los puntos m ́as cercanos aplique un filtro de
+# mınimo. 
 
 # Umbral
 umbral = 10
-# Mascara
-mask = img_fft_log > umbral
+img_fft_log[img_fft_log < umbral] = 1
+img_fft_log[img_fft_log >= umbral] = 0
+
 # Filtro de mínimo
-mask = filters.minimum_filter(mask, size=3)
+img_fft_log = filters.minimum_filter(img_fft_log, size=3)
 
+# Mostrar la máscara después del filtro de mínimo
+plt.imshow(img_fft_log, cmap='gray')
+plt.title('Máscara Umbral')
+plt.axis('off')
+plt.show()
 
+# Con la m ́ascara del filtro selectivo suprima adem ́as las componentes de muy baja frecuencia.
+# Para esto puede combinar la m ́ascara obtenida en el paso anterior (c) con un filtro pasa bajo
+# ideal de radio a elecci ́on (utilizar operaciones l ́ogicas). Para la implementaci ́on del filtro ideal
+# utilice la funci ́on disponible en el archivo fft lib. Despliegue las tres im ́agenes m ́ascara, filtro
+# ideal y combinaci ́on de ambos,
 
+# Filtro pasa bajo ideal
+def filtro_ideal(img, D0):
+    # Dimensiones de la imagen
+    M, N = img.shape
+    # Coordenadas del centro
+    u0 = M/2
+    v0 = N/2
+    # Filtro ideal
+    filtro = np.zeros((M,N))
+    for u in range(M):
+        for v in range(N):
+            D = np.sqrt((u-u0)**2 + (v-v0)**2)
+            if D <= D0:
+                filtro[u,v] = 1
+    return filtro
+
+# Filtro ideal
+D0 = 50
+filtro = filtro_ideal(img, D0)
+plt.imshow(filtro, cmap='gray')
+plt.title('Filtro ideal')
+plt.axis('off')
+plt.show()
+
+# Combinación de ambos
+img_fft_log = filtro* img_fft_log
+plt.imshow(img_fft_log, cmap='gray')
+plt.title('Combinación')
+plt.axis('off')
+plt.show()
 
